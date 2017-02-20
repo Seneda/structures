120be8cb4ad34ac58394cc88e7c4483ccc5be117
Seneda <senedanassirali@gmail.com>
2017-02-20 21:51:15 +0000

    Remove old Lamination class

--- src/glassBallustrade.java
+++ src/glassBallustrade.java
@@ -2,7 +2,6 @@
  * Created by seneda on 18/02/17.
  */
 import com.seneda.structures.FormGlassCantilever;
-import com.seneda.structures.FormSelectLamination;
 
 import javax.swing.*;
 
@@ -10,11 +9,9 @@ public class glassBallustrade {
     JFrame frame = new JFrame("Glass Balustrade");
     JTabbedPane tabbedPane = new JTabbedPane();
     JPanel panelMinThicks = new FormGlassCantilever().getMainPanel();
-    JPanel panelEffThicknesses = new FormSelectLamination().getMainPanel();
 
     public glassBallustrade(){
         tabbedPane.add("Min Thicknesses", panelMinThicks);
-        tabbedPane.add("Effective Thicknesses", panelEffThicknesses);
 
         frame.getContentPane().add(tabbedPane);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
